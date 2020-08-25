6cf430a28472fdfabc3e0448fdb090e06b27ef7e
Harry Martland <HarryEMartland@gmail.com>
2020-08-25 19:30:26 +0100

    fixed bootstrap js dependencies

--- middleware/helmetConfiguration.js
+++ middleware/helmetConfiguration.js
@@ -3,7 +3,7 @@ let helmet = require('helmet');
 module.exports = helmet({
     contentSecurityPolicy: {
         directives: {
-            "default-src": ["'self'", "https://stackpath.bootstrapcdn.com"]
+            "default-src": ["'self'", "https://stackpath.bootstrapcdn.com", "https://cdn.jsdelivr.net", "https://code.jquery.com"]
         }
     }
 })
